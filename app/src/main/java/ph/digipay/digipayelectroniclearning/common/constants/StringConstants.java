69db0b954349b9c5d7017cb2bf1cd39010e1a46c
jomaribenito <jomari.benito@digipay.ph>
2018-12-20 18:03:11 +0800

    String constants for database reference

--- app/src/main/java/ph/digipay/digipayelectroniclearning/common/constants/StringConstants.java
+++ app/src/main/java/ph/digipay/digipayelectroniclearning/common/constants/StringConstants.java
@@ -4,4 +4,12 @@ public class StringConstants {
     public static final String CORRECT_ANSWERS = "correct_answer";
     public static final String ANSWER_LIST = "answer_list";
     public static final String QUESTIONNAIRE_FORMS = "questionnaire_forms";
+    public static final String TYPE_ADMIN = "admin";
+    public static final String TYPE_USER = "user";
+    public static final String PDF_URL = "pdf_url";
+    public static final String MODULE_DB = "module";
+    public static final String QUESTIONNAIRE_DB = "questionnaire";
+    public static final String USERS_DB = "users";
+    public static final String PDF_LIST_DB = "pdf_list";
+    public static final String VIDEO_LIST_DB = "video_list";
 }
