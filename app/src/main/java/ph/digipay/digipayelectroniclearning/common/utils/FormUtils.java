87494b1191a99bf7237790f2f89409aecebd6bcc
jomaribenito <jomari.benito@digipay.ph>
2018-12-20 18:02:30 +0800

    Add getting text of spinner selected item

--- app/src/main/java/ph/digipay/digipayelectroniclearning/common/utils/FormUtils.java
+++ app/src/main/java/ph/digipay/digipayelectroniclearning/common/utils/FormUtils.java
@@ -1,9 +1,11 @@
 package ph.digipay.digipayelectroniclearning.common.utils;
 
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class FormUtils {
     public static String getTrimmedString(TextView tv){
         return tv.getText().toString().trim();
     }
+    public static String getSpinnerItemString(Spinner spinner) { return spinner.getSelectedItem().toString().trim(); }
 }
